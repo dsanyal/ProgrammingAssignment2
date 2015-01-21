bcb5eba8caa44970bc6bbd93a950c42b221928cb
Debashis Sanyal <dsanyal@astro.uni-bonn.de>
2015-01-21 12:32:07 +0100

    Programming assignment 2 of the Coursera course R programming

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#The makeCacheMatrix takes a matrix as its argument and stores its inverse 
+#in the cache (i.e in the containing environement which is global in this case). 
+#It returns a special 'matrix' which is basically a list containing the containing the 
+#functions to get information about the input matrix 'x'.
 
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv_cache<-NULL
+  get_matrix<-function() x
+  set_inverse<-function(inverse){
+    inv_cache<<-inverse
+  }
+  get_inverse <- function() inv_cache
+  list( get_matrix = get_matrix, set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
 ## Write a short comment describing this function
 
+#The cacheSolve function computes the inverse of its argument (the special 'matrix') using solve() 
+#if the corresponding inverse does not already exist in the cache.
+#If the inverse already exists in the cache, then 
+#  it is retrieved from the cache by the get_inverse function 
+# with an appropriate message.
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse<-x$get_inverse()
+  if(!is.null(inverse)){
+    message("Retrieving the inverse from cache")
+    return(inverse)
+  }
+  input_matrix<-x$get_matrix()
+  inverse<-solve(input_matrix,...)
+  x$set_inverse(inverse)
+  inverse
 }
